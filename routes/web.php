d0ffee03377f738fdd6c6b0666bae3f581e718b3
szantog <szantog82@gmail.com>
2022-11-05 09:19:39 +0100

    Adds main routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserOrdersController;
 
 /*
  * |--------------------------------------------------------------------------
@@ -12,7 +18,15 @@ use Illuminate\Support\Facades\Route;
  * |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name("home");
+
+Route::get('/register', [RegisterController::class, 'index'])->name("register");
+
+Route::get('/login', [LoginController::class, 'index'])->name("login");
+
+Route::get('/logout', [LogoutController::class, 'index'])->name("logout");
+
+Route::get('/my_orders', [UserOrdersController::class, 'index'])->name("my_orders");
+
+Route::get('/order_hoodies', [OrderController::class, 'index'])->name("order_hoodies");
 
