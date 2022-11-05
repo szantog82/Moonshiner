0d45c45c3f7638ec543913d023f1397d077b1ccd
szantog <szantog82@gmail.com>
2022-11-05 11:05:34 +0100

    Adds function in Order Controller to show order view blade file

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class OrderController extends Controller
 {
-    //
+    public function index() {
+        return view('order', ["items" => Item::all()]);
+    }
 }
