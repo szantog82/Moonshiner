b9c696df69464c844bd97ee3fd05ca3630ba4e43
szantog <szantog82@gmail.com>
2022-11-05 10:02:38 +0100

    Fixes user table name and puts is_valid field to fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'user';
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_valid'
     ];
 
     /**
