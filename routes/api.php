71ee086bdda721eb858f3e114662f989881f3bc5
szantog <szantog82@gmail.com>
2022-11-08 09:33:47 +0100

    Adds API routes in connection with orders

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post("/check_cart", [OrderController::class, "checkCart"])->name("item.check.cart");
+Route::post("/add_to_cart", [OrderController::class, "addItemToCart"])->name("item.add.cart");
+Route::post("/upload_promocode", [OrderController::class, "setPromoCode"])->name("order.upload.promocode");
+Route::post("/change_item_count_cart", [OrderController::class, "changeItemCountInCart"])->name("item.changecount.cart");
+Route::post("/remove_from_cart", [OrderController::class, "removeItemFromCart"])->name("item.remove.fromcart");
+
+Route::post('/store_order', [OrderController::class, "storeOrder"])->name("store.order");
