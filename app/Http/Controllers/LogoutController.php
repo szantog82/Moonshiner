2ab88cf7da82b081015dced915ae61798dc57a9c
szantog <szantog82@gmail.com>
2022-11-08 09:59:17 +0100

    Adds session forgetting to Logout controller

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -1,13 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    public function logoutUser() {
+
+    public function logoutUser()
+    {
         auth()->logout();
+        session()->forget('order.items');
+        session()->forget("promoCode");
         return redirect()->to(route('home'));
     }
 }
