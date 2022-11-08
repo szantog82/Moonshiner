efa5396f4e5d0445955a6abfbb5804b576be3322
szantog <szantog82@gmail.com>
2022-11-08 09:27:00 +0100

    Adds db seeders with 6 hoodies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,11 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
     /**
      * Seed the application's database.
      *
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ItemSeeder::class
+        ]);
     }
 }
