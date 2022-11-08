8af3efa1dc5c2f9f23102529bbeb8d553a22f55d
szantog <szantog82@gmail.com>
2022-11-08 10:01:43 +0100

    Adds some styles to main layout

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -1,12 +1,15 @@
 <!doctype html>
 <html>
 <head>
+	<title>Moonshiner hoodie ordering app</title>
+	<link rel="icon" href="https://moonshiner.at/wp-content/uploads/2022/07/moonshiner-logo-POS-1000x203px.png" type = "image/x-icon">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" crossorigin="anonymous"></script>
    @yield('head')
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body style="padding-top: 4.5rem;">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
                 <img height="40" src="https://moonshiner.at/wp-content/uploads/2022/07/moonshiner-logo-POS-1000x203px.png" />
