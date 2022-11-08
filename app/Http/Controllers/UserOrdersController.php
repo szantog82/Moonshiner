fe7529fb5f3abc0aa701fd03b3b781ceffacfc0b
szantog <szantog82@gmail.com>
2022-11-08 10:04:32 +0100

    Adds index route to User Orders Controller

--- app/Http/Controllers/UserOrdersController.php
+++ app/Http/Controllers/UserOrdersController.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class UserOrdersController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $data = Order::select("order.id", "item.name", "order.count", "order.created_at")->join("item", "order.item_id", "=", "item.id")->where("user_id", auth()->user()->id)->get();
+        return view('my_orders', [
+            "data" => $data
+        ]);
+    }
 }
